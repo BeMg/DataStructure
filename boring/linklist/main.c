994cf7d35228c0d7e4c0cf02cc7279c26c488322
BeMg <gccbg04538@gmail.com>
2015-10-15 15:18:02 +0800

    add linklist insert and printall

--- boring/linklist/main.c
+++ boring/linklist/main.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+struct Node{
+    int num;
+    struct Node* next;
+};
 
 typedef struct Node Node;
 
-typedef struct {
-    int num;
-    struct Node* next;
-}Node;
-
-bool insert(Node* node, int num){
-    Node* ptr;
+bool insert(Node** node, int num){
+    Node* ptr = (Node*) malloc(sizeof(Node));
     if(ptr == NULL)
 	return false;
-    ptr -> next = node;
+    ptr -> next = *node;
     ptr -> num = num;
-    node = ptr;
+    *node = ptr;
+    return true;
 }
 
 void print_all_list(Node* node){
-    if(*node){
+    while(node!=NULL){
 	printf("%d ",node -> num);
 	node = node -> next;
     }
+    puts("");
 }
 
 
 int main(){
-    Node* start;
-    insert(start,1);   
-    insert(start,2);   
-    insert(start,3);
+    Node* start = NULL;
+    insert(&start,1);
+    insert(&start,2);   
+    insert(&start,3);
     print_all_list(start);
-
+    return 0;
 }
