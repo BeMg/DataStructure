3b54e458cd1858c3cb7907e41e7824593697bd4e
BeMg <gccbg04538@gmail.com>
2015-11-03 21:15:02 +0800

    insert ifn

--- pro2/main.c
+++ pro2/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-typedef struct node Node;
+#include <stdlib.h>
 
 struct node {
     int data;
@@ -8,29 +7,63 @@ struct node {
     struct node *right;
 };
 
+typedef struct node Node;
 
 Node* search(Node *root , int k){
     if(!root) return NULL;
-    if(root->data == k) return &(root->data);
+    if(root->data == k) return (Node*) &(root->data);
     if(root->data < k)
 	return search(root->left,k);
     return search(root->right,k);
 }
 
 
-void insert(struct node *root, int k){
-
+Node* search_terminal(Node *root , int k){
+    while(root){
+	if(k == root->data) return NULL;
+	if(k < root->data){
+	    if(!(root->left))
+		return root;
+	    root = root->left;
+	}else{ 
+	    if(!(root->right))
+		return root;
+	    root = root->right;
+	}
+    }
+    return NULL;
 }
 
-
-
+void insert(Node **root, int k){
+    Node *ptr = (Node*) malloc(sizeof(Node)),*temp = search_terminal(*root,k);
+    if(temp || !(*root)){
+	ptr->data = k;
+	ptr->right = ptr->left = NULL;
+	if(*root)
+	    if(k > ptr->data)
+		temp->right = ptr;
+	    else
+		temp->left = ptr;
+	else
+	    *root = ptr;
+    }
+    else{
+	printf("Error.The number is already in tree.\n");
+    }
+}
 
 
 
 int main(){
     Node *root;
     root = NULL;
-    
-
+    insert(&root,20);
+    insert(&root,15);
+    insert(&root,10);
+    insert(&root,12);
+    insert(&root,25);
+    insert(&root,22);
+    if(!(search(root,10)))
+	printf("YA!\n");
     return 0;
 }
