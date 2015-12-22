130106466658f189d168d7195105cc5272f34091
BeMg <gccbg04538@gmail.com>
2015-12-22 19:12:45 +0800

    pro4 init

---
+++ pro4/main.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+
+typedef struct {
+    int ID;
+    char FirstName[15];
+    char LastName[15];
+    char Gender;
+    int Age;
+    char PhoneNum[18];
+}Date;
+
+int main(){
+    
+}
