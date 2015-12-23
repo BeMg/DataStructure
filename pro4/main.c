6704e0684e3faa9598a2b9bb2a372c32e23f604f
BeMg <gccbg04538@gmail.com>
2015-12-23 09:55:23 +0800

    add qsort

--- pro4/main.c
+++ pro4/main.c
@@ -24,6 +24,37 @@ typedef struct {
 Date data[10000];
 int data_cnt=0;
 
+void swap(void *a,void *b,size_t size){
+    void* temp = malloc(size);
+    memcpy(temp,a,size);
+    memcpy(a,b,size);
+    memcpy(b,temp,size);
+    free(temp);
+}
+
+int partition(void *a,int len,int pivot_idx,size_t size,int (*cmp)(const void*a,const void*b)){
+        int i;
+        swap(a+pivot_idx*size,a+(len-1)*size,size);
+        int storeindex = 0;
+        for(i=0; i<len-1; i++){
+            if(cmp(a+i*size,a+(len-1)*size)<0){
+                swap(a+i*size,a+storeindex*size,size);
+                storeindex++;
+            }
+        }
+        swap(a+(len-1)*size,a+storeindex*size,size);
+        return storeindex;
+}
+
+void quick_sort(void *a,int len,size_t size,int (*cmp)(const void*a,const void*b)){
+    srand(time(NULL));
+    if(len==0 || len == 1) return;
+    int pivot = rand()%len;
+    int small_len = partition(a,len,pivot,size,cmp);
+    quick_sort(a,small_len,size,cmp);
+    quick_sort(a+(small_len+1)*size,len-small_len-1,size,cmp);
+}
+
 /*inital all variable*/
 
 void variable_init() {
